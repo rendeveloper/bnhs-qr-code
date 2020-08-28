de6e7f729bf8f0df2b19fe01ee5c2394bb7982db
Laurence Verallo <laurence_verallo@yahoo.com>
2020-08-28 19:18:46 +0800

    adding vuetify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,35 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import initialize from './initialize'
 
-createApp(App).use(store).use(router).mount('#app')
+import VueAwesome from 'vue-awesome'
+import VueImg from 'v-img'
+import Element from 'element-ui'
+import Multiselect from 'vue-multiselect'
+import VuejQuery from 'vue-jquery'
+import jQuery from 'jquery'
+
+import 'font-awesome/css/font-awesome.min.css'
+import vuetify from './plugins/vuetify';
+
+global.jQuery = jQuery
+global.$ = jQuery
+
+initialize()
+
+Vue.config.productionTip = false
+
+Vue.use(VueImg)
+Vue.use(VuejQuery)
+Vue.component('icon', VueAwesome)
+Vue.component('multiselect', Multiselect)
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
