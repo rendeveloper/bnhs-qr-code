c28fb63c8d2026cc802c2f88d30cf9637873e04f
Laurence Verallo <laurence_verallo@yahoo.com>
2020-09-16 00:42:59 +0800

    QR-6: Create Asp.net SPA backend api and integrate frontend

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-# bnhs-qr-code
+## BNHS QR Code
+Please make sure to install nodejs in your machine. Please refer to [node.js](http://nodejs.org/) to download nodejs.
+
+## Building BNHS QR Code
+* Open `bnhs-qr-code.sln` solution in **visual studio**
+* Please configure the app config in Content Root Path `bnhs-qr-code/appsettings.json`
+* Run the solution by pressing `ctrl + F5`
+
+## Sample config file
+```json
+{
+  
+}
+
+```
+#
+# BNHS QR Code
+
+> Please make sure to install nodejs in your machine. Please refer to [node.js](http://nodejs.org/) to download nodejs.
 
 ## Project setup
 ```
@@ -15,5 +33,17 @@ npm run serve
 npm run build
 ```
 
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+* Run the application
---
+++ bnhs-qr-code/Startup.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VueCliMiddleware;
+
+namespace bnhs_qr_code
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin", builder =>
+                    builder.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowAnyOrigin()
+                        .AllowCredentials()
+                );
+            });
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseAuthentication();
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+
+            app.UseCors("AllowSpecificOrigin");
+            //Remove HTTPS redirect middleware for development
+            //otherwise it’s necessary to configure ASP.NET Core SSL certificate for a Vue app
+            //app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    // run npm process with client app
+                    spa.UseVueCli(npmScript: "serve", port: 8080);
+                }
+            });
+        }
+    }
+}
