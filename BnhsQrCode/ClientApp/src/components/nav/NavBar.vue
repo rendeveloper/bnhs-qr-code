68ea7cd5e52091ba93933479f5527841065ae954
Laurence Verallo <laurence_verallo@yahoo.com>
2020-10-19 18:03:29 +0800

    QR-9: Add History button and show list

--- BnhsQrCode/ClientApp/src/components/nav/NavBar.vue
+++ BnhsQrCode/ClientApp/src/components/nav/NavBar.vue
@@ -19,14 +19,85 @@
       </div>
 
       <v-spacer></v-spacer>
-
-      <v-btn
-        text
-      >
-        <span class="mr-2" style="letter-spacing: 2px;"><strong>Logout</strong></span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer> 
+        <v-text-field
+          label="Track Your Time, Enter your teacher id"
+          single-line
+          v-model="teacherId"
+          hide-details
+          clearable
+        ></v-text-field>
+        <v-btn
+          text
+          @click="getUser()"
+        >
+          <span class="mr-2" style="letter-spacing: 2px;"><strong>Track</strong></span>
+          <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
+    
+    <v-row justify="center">
+      <v-dialog
+        v-model="dialogTime"
+        max-width="600px"
+      >
+        <v-card>
+          <v-card-title>
+            <v-icon class="mr-1">mdi-timetable</v-icon>Daily Time Table
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="searchTime"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+              clearable
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+            :headers="headersTime"
+            :items="itemsTime"
+            :search="searchTime"
+            :loading="timeTblLoading" 
+            loading-text="Loading... Please wait"
+          >
+          <template v-slot:header.bodyTemp="{ header }">
+            <strong>{{ header.text }}</strong> <v-icon small>mdi-temperature-celsius</v-icon>
+          </template>
+          <template v-slot:item.bodyTemp="{ item }">
+            <strong>{{ item.bodyTemp }}</strong>
+          </template>
+          <template v-slot:item.timeStatus="{ item }">
+            <v-chip
+              :color="getColorTime(item.timeStatus)"
+              label
+              dark
+            >
+              {{ item.timeStatus.toUpperCase() }}
+            </v-chip>
+          </template>
+          <template v-slot:item.createdByDateTime="{ item }">
+            <strong>{{ item.createdByDateTime }}</strong>
+          </template>
+          </v-data-table>
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+            color="primary"
+            text
+            @click="dialogTime = false"
+          >
+            Close
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
   </v-container>
 </template>
 
@@ -35,6 +106,20 @@
 
   export default {
     name: 'NavBar',
+    data() {
+      return {
+        teacherId: "",
+        dialogTime: false,
+        timeTblLoading: false,
+        searchTime: '',
+        headersTime: [
+          { text: 'Date & Time', value: 'createdByDateTime' },
+          { text: 'Status', value: 'timeStatus' },
+          { text: 'Body temperature', align: 'start', value: 'bodyTemp'}
+        ],
+        itemsTime: []
+      }
+    },
     computed: {
       ...mapState({
         drawer: state => state.drawer.isOpen
@@ -44,8 +129,38 @@
       ...mapActions({
         openDrawer: 'OPEN_DRAWER'
       }),
+      ...mapActions(['getScanQRCode', 'getAllTimeUser']),
       isPopUp(){
         this.openDrawer(!this.drawer)
+      },
+      async getUser(){
+        var self = this
+        self.itemsTime = []
+        self.dialogTime = true
+        self.timeTblLoading = true
+        var scanQRCode = {
+          teacherId: self.teacherId
+        }
+        await self.getScanQRCode(scanQRCode).then(async (response) => {
+          await self.getAllTimeUser(response.data.id).then(x => {
+            self.itemsTime = x.data
+            self.timeTblLoading = false
+          }).catch(() => {
+            self.$message({
+                message: "Please enter a valid teacher id",
+                type: "error"
+            })
+          })
+        }).catch(() => {  
+          self.$message({
+            message: "An unexpected error occurred",
+            type: "error"
+          })
+        })
+      },
+      getColorTime(timeStatus) {
+        if (timeStatus == 'In') return '#4CAF50'
+        else return '#FF5252'
       }
     }
   }
--- BnhsQrCode/ClientApp/src/components/user/AllUsers.vue
+++ BnhsQrCode/ClientApp/src/components/user/AllUsers.vue
@@ -2,7 +2,7 @@
   <v-container id="user-profile" fluid tag="section" style="margin-top: 100px;">
     <v-row justify="center">
       <v-col cols="12" md="11">
-        <v-data-table :headers="headers" :items="items" sort-by="firstName" :loading="tblLoading" loading-text="Loading... Please wait" class="elevation-1">
+        <v-data-table :headers="headers" :items="items" :search="searchTbl" sort-by="firstName" :loading="tblLoading" loading-text="Loading... Please wait" class="elevation-1">
           <template v-slot:top>
             <v-toolbar flat >
               <v-card icon="mdi-clipboard-text" :elevation="8" color="#217BD4" class="mb-n6 pa-7" style="top: -25px; margin-right: 1%;">
@@ -14,6 +14,16 @@
                 inset
                 vertical
               ></v-divider>
+              <v-text-field
+                v-model="searchTbl"
+                append-icon="mdi-magnify"
+                label="Search"
+                single-line
+                hide-details
+                clearable
+              ></v-text-field>
+              <v-spacer></v-spacer>
+              <v-spacer></v-spacer>
               <v-spacer></v-spacer>
               <v-btn color="primary" dark class="mb-2 mt-1" :loading="btnLoading" @click="newItem()">
                 New Item
@@ -21,34 +31,123 @@
             </v-toolbar>
           </template>
           <template v-slot:item.actions="{ item }">
-            <v-icon
-              medium
-              class="mr-2"
-              @click="editItem(item)"
-            >
-              mdi-pencil
-            </v-icon>
-            <v-icon
-              medium
-              @click="deleteItem(item)"
-            >
-              mdi-delete
-            </v-icon>
-          </template>
-          <!-- <template v-slot:actions-extras>
             <v-slide-x-reverse-transition>
-            <v-tooltip left >
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn icon class="my-0" v-bind="attrs" v-on="on" >
-                  <v-icon>mdi-refresh</v-icon>
-                </v-btn>
-              </template>
-              <span>Refresh form</span>
-            </v-tooltip>
-          </v-slide-x-reverse-transition>
-          </template> -->
+              <v-tooltip
+                bottom
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    icon
+                    v-bind="attrs"
+                    class="my-0"
+                    @click.stop="timeItem(item)"
+                    v-on="on"
+                  >
+                    <v-icon>mdi-timetable</v-icon>
+                  </v-btn>
+                </template>
+                <span>Time Table</span>
+              </v-tooltip>
+            </v-slide-x-reverse-transition>
+            <v-slide-x-reverse-transition>
+              <v-tooltip
+                bottom
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    icon
+                    v-bind="attrs"
+                    class="my-0"
+                    @click="editItem(item)"
+                    v-on="on"
+                  >
+                    <v-icon>mdi-pencil</v-icon>
+                  </v-btn>
+                </template>
+                <span>Edit</span>
+              </v-tooltip>
+            </v-slide-x-reverse-transition>
+            <v-slide-x-reverse-transition>
+              <v-tooltip
+                bottom
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    icon
+                    v-bind="attrs"
+                    class="my-0"
+                    @click="deleteItem(item)"
+                    v-on="on"
+                  >
+                    <v-icon>mdi-delete</v-icon>
+                  </v-btn>
+                </template>
+                <span>Delete</span>
+              </v-tooltip>
+            </v-slide-x-reverse-transition>
+          </template>
         </v-data-table>
       </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-dialog
+        v-model="dialogTime"
+        max-width="600px"
+      >
+        <v-card>
+          <v-card-title>
+            <v-icon class="mr-1">mdi-timetable</v-icon>Daily Time Table
+            <v-spacer></v-spacer>
+            <v-text-field
+              v-model="searchTime"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+              clearable
+            ></v-text-field>
+          </v-card-title>
+          <v-data-table
+            :headers="headersTime"
+            :items="itemsTime"
+            :search="searchTime"
+            :loading="timeTblLoading" 
+            loading-text="Loading... Please wait"
+          >
+          <template v-slot:header.bodyTemp="{ header }">
+            <strong>{{ header.text }}</strong> <v-icon small>mdi-temperature-celsius</v-icon>
+          </template>
+          <template v-slot:item.bodyTemp="{ item }">
+            <strong>{{ item.bodyTemp }}</strong>
+          </template>
+          <template v-slot:item.timeStatus="{ item }">
+            <v-chip
+              :color="getColorTime(item.timeStatus)"
+              label
+              dark
+            >
+              {{ item.timeStatus.toUpperCase() }}
+            </v-chip>
+          </template>
+          <template v-slot:item.createdByDateTime="{ item }">
+            <strong>{{ item.createdByDateTime }}</strong>
+          </template>
+          </v-data-table>
+          <v-divider></v-divider>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+            color="primary"
+            text
+            @click="dialogTime = false"
+          >
+            Close
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-row>
   </v-container>
 </template>
@@ -62,6 +161,7 @@
       return {      
         btnLoading: false,
         tblLoading: false,
+        searchTbl: '',
         headers: [
           { text: 'Teacher Id', align: 'start', value: 'teacherId' },
           { text: 'First name', value: 'firstName' },
@@ -74,7 +174,16 @@
           { text: 'Grade / Admin / Principal / Role', value: 'role' },
           { text: 'Actions', value: 'actions', sortable: false },
         ],
-        items: []
+        items: [],
+        dialogTime: false,
+        timeTblLoading: false,
+        searchTime: '',
+        headersTime: [
+          { text: 'Body temperature', align: 'start', value: 'bodyTemp'},
+          { text: 'Status', value: 'timeStatus' },
+          { text: 'Date & Time', value: 'createdByDateTime' }
+        ],
+        itemsTime: []
       }
     },
     created(){
@@ -88,7 +197,7 @@
     watch: {
     },
     methods: {
-      ...mapActions(['getAllUsers', 'deleteUser']),
+      ...mapActions(['getAllUsers', 'deleteUser', 'getAllTimeUser']),
       async initialize () {
         var self = this
         self.tblLoading = true
@@ -176,6 +285,25 @@
           this.$router.push('/user/create')
           this.btnLoading = false
         }, 500)
+      },
+      async timeItem(item){
+        var self = this
+        self.itemsTime = []
+        self.dialogTime = true
+        self.timeTblLoading = true
+        await self.getAllTimeUser(item.id).then(response => {
+          self.itemsTime = response.data
+          self.timeTblLoading = false
+        }).catch(() => {
+          self.$message({
+              message: "An unexpected error occurred",
+              type: "error"
+          })
+        })
+      },
+      getColorTime(timeStatus) {
+        if (timeStatus == 'In') return '#4CAF50'
+        else return '#FF5252'
       }
     }
   }
